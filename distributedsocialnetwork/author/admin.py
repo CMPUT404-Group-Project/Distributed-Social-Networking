09992e228292e96c0188ccc96e43c5400a2f2301
Ken Li <krli@ualberta.ca>
2020-02-23 02:51:42 -0700

    Auther model working

--- distributedsocialnetwork/author/admin.py
+++ distributedsocialnetwork/author/admin.py
@@ -1,3 +1,18 @@
+from django.contrib.auth.admin import UserAdmin
 from django.contrib import admin
+from .models import Author
 
-# Register your models here.
+
+class AuthorAdmin(UserAdmin):
+    model = Author
+
+    list_display = ('displayName', 'first_name', 'last_name', 'email', 'is_active', 'is_staff')
+    search_fields = ('displayName', 'first_name', 'last_name', 'email')
+    readonly_fields = ('id', 'host', 'url', 'displayName', 'date_joined', 'last_login', )
+
+    filter_horizontal = ()
+    ordering = ()
+    list_filter = ()
+    fieldsets = ()
+
+admin.site.register(Author, AuthorAdmin)
--- distributedsocialnetwork/author/models.py
+++ distributedsocialnetwork/author/models.py
@@ -1,20 +1,86 @@
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 from django.db import models
 import uuid
 
-class Author(AbstractUser):
-    # Inheriting Django's AbstractUser
-    # See https://docs.djangoproject.com/en/3.0/ref/contrib/auth/
 
-    # Overwritting Django AbstractUser Fields
-    email = models.EmailField(blank=True)
+class AuthorManager(BaseUserManager):
+    def create_user(self, displayName, first_name, last_name, email, password=None):
+        if not displayName:
+            raise ValueError('Display name is required.')
+        if not first_name:
+            raise ValueError('First name is required.')
+        if not last_name:
+            raise ValueError('Last name is required.')
+        if not email:
+            raise ValueError('Email is required.')
 
-    # Custom Fields
+        user = self.model(
+            displayName = displayName,
+            first_name = first_name,
+            last_name = last_name,
+            email = self.normalize_email(email)
+            )
+
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, displayName, first_name, last_name, email, password):
+        user = self.create_user(
+            displayName = displayName,
+            password = password,
+            first_name = first_name,
+            last_name = last_name,
+            email = self.normalize_email(email)
+            )
+
+        user.is_active = True
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+        
+        return user
+
+
+class Author(AbstractBaseUser):
+    # Inheriting Django's AbstractBaseUser
+    # See https://docs.djangoproject.com/en/3.0/topics/auth/customizing/
+    # Also see https://docs.djangoproject.com/en/3.0/ref/contrib/auth/
+
+    # Required Fields
     generatedUUID = uuid.uuid4().hex
     currentHost = 'https://dsnfof.herokuapp.com/'
 
-    id = models.CharField(max_length=32, primary_key=True, default=generatedUUID, editable=False, unique=True)
-    host = models.CharField(max_length=30, default=currentHost, editable=False)
-    displayName = models.CharField(max_length=150, blank=False)
-    url = models.CharField(max_length=70, default=currentHost + 'author/' + str(generatedUUID), editable=False)
-    github = models.CharField(max_length=150, blank=True)
+    id = models.CharField(max_length=32, default=generatedUUID, editable=False, unique=True, primary_key=True)  # Generated
+    host = models.CharField(max_length=30, default=currentHost, editable=False)                                 # Generated
+    url = models.CharField(max_length=70, default=currentHost + 'author/' + str(generatedUUID), editable=False) # Generated
+    displayName = models.CharField(max_length=150, blank=False, unique=True)                                    # Required
+    github = models.CharField(max_length=255, blank=True)                                                       # Optional
+
+    # Other Fields
+    first_name = models.CharField(max_length=30, blank=False)                                                   # Required
+    last_name = models.CharField(max_length=150, blank=False)                                                   # Required
+    email = models.EmailField(max_length=255, blank=False, unique=True)                                         # Required
+
+    is_active = models.BooleanField(default=False)                                                              # Generated
+    is_staff = models.BooleanField(default=False)                                                               # Generated
+    is_superuser = models.BooleanField(default=False)                                                           # Generated
+
+    date_joined = models.DateField(auto_now_add=True)                                                           # Generated
+    last_login = models.DateField(auto_now=True)                                                                # Generated
+
+    USERNAME_FIELD = 'displayName'
+    EMAIL_FIELD = 'email'
+    REQUIRED_FIELDS = ['first_name', 'last_name', 'email']
+
+    objects = AuthorManager()
+
+    def __str__(self):
+        return self.displayName
+
+    def has_perm(self, perm, obj=None):
+        return self.is_staff
+
+    def has_module_perms(self, app_label):
+        return self.is_staff
