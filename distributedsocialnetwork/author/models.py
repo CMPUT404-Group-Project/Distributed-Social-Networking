76ca72f2e512735958c644ed34320d53e39d02cd
Ken Li <krli@ualberta.ca>
2020-02-22 22:10:17 -0700

    Added custom fields to author

--- distributedsocialnetwork/author/models.py
+++ distributedsocialnetwork/author/models.py
@@ -1,3 +1,20 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+import uuid
 
-# Create your models here.
+class Author(AbstractUser):
+    # Inheriting Django's AbstractUser
+    # See https://docs.djangoproject.com/en/3.0/ref/contrib/auth/
+
+    # Overwritting Django AbstractUser Fields
+    email = models.EmailField(blank=True)
+
+    # Custom Fields
+    generatedUUID = uuid.uuid4().hex
+    currentHost = 'https://dsnfof.herokuapp.com/'
+
+    id = models.CharField(max_length=32, primary_key=True, default=generatedUUID, editable=False, unique=True)
+    host = models.CharField(max_length=30, default=currentHost, editable=False)
+    displayName = models.CharField(max_length=150, blank=False)
+    url = models.CharField(max_length=70, default=currentHost + 'author/' + str(generatedUUID), editable=False)
+    github = models.CharField(max_length=150, blank=True)
--- distributedsocialnetwork/distributedsocialnetwork/settings.py
+++ distributedsocialnetwork/distributedsocialnetwork/settings.py
@@ -38,7 +38,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'author',
 ]
+
+AUTH_USER_MODEL = 'author.Author'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
