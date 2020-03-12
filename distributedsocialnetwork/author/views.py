800f43693c77ea4b95f0eb0371033aa3245c8332
Anders Johnson <anders.johnson.email@gmail.com>
2020-03-11 22:48:38 -0600

    First WIP of the new author page. Now pulls in relevant posts depending on the user that is logged in. TODO: Hook button up to send friend request

--- distributedsocialnetwork/author/views.py
+++ distributedsocialnetwork/author/views.py
@@ -5,6 +5,7 @@ from django.urls import reverse_lazy
 from .forms import AuthorCreationForm, AuthorChangeForm, AuthorAuthenticationForm
 from .models import Author
 from post.models import Post
+from friend.models import Friend
 
 
 def index(request):
@@ -90,5 +91,32 @@ def logout_author(request):
 def view_author(request, pk):
     context = {}
     context['author'] = get_object_or_404(Author, id__icontains=pk)
-    context['posts'] = Post.objects.filter(author=context['author'].id)
+    if request.user.is_authenticated:
+        context["user"] = request.user
+        # We will show different posts depending on if the user is logged in and authenticated
+        if request.user.id == context["author"].id:
+            # The user is logged in, so they should be able to see all of their posts
+            context["posts"] = Post.objects.filter(author=context["author"].id)
+        else:
+            # We return the posts this user can see
+            author_public_posts = Post.objects.filter(
+                author=context["author"].id, visibility="PUBLIC")
+            author_private_posts = Post.objects.filter(
+                author=context["author"].id, visibility="PRIVATE", visibleTo__icontains=request.user.id)
+            post_query_set = author_private_posts | author_public_posts
+            if Friend.objects.are_friends(context["author"], request.user):
+                # They are friends, so they can see some other posts
+                serveronly_posts = Post.objects.filter(
+                    visibility="SERVERONLY", author=context["author"].id)
+                friend_posts = Post.objects.filter(
+                    visibility="FRIENDS", author=context["author"].id)
+                # They are friends, so they have to get FOAF posts
+                foaf_posts = Post.objects.filter(
+                    visibility="FOAF", author=context["author"].id)
+                post_query_set = post_query_set | serveronly_posts | friend_posts | foaf_posts
+            context["posts"] = post_query_set
+    else:
+        context['posts'] = Post.objects.filter(
+            author=context['author'].id, visibility="PUBLIC")
+        context["user"] = None
     return render(request, 'detailed_author.html', context)
