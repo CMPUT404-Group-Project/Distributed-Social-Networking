d1571dcf77424807674bbe94199525ab2eb3f1bb
Anders Johnson <anders.johnson.email@gmail.com>
2020-03-22 18:01:07 -0600

    Added a method to pull public posts from other servers

--- distributedsocialnetwork/post/retrieval.py
+++ distributedsocialnetwork/post/retrieval.py
@@ -4,6 +4,7 @@ from author.models import Author
 from post.serializers import PostSerializer, CommentSerializer
 from author.serializers import AuthorSerializer
 import requests
+import datetime
 
 # The purpose of this is to provide functions that can import Posts from the other Nodes.
 # Each Node may not be completely to spec, so we may have to add server-specific compatibility here and there.
@@ -17,10 +18,6 @@ def sanitize_author(obj):
         obj["displayName"] = obj["display_name"]
         del obj["display_name"]
 
-    # Polar Bear sanitizers
-    if "github" in obj.keys():
-        if obj["github"] is None:
-            obj["github"] = ""
     return obj
 
 
@@ -28,11 +25,8 @@ def sanitize_post(obj):
     # First thing we do is set the author to just the id of the author. this makes it deserializable
     obj["author"] = obj["author"]["id"]
 
-    # Polar Bear sanitizers
-    if "description" in obj.keys():
-        if obj["description"] is None:
-            obj["description"] = "t"
-
+    obj["visibleTo"] = ','.join(obj["visibleTo"])
+    obj["categories"] = ','.join(obj["visibleTo"])
     return obj
 
 
@@ -40,13 +34,17 @@ def get_public_posts():
     # Returns a QuerySet of all the public posts retrieved from http://service/posts
     print("running")
     nodes = list(Node.objects.all())
+    public_posts = Post.objects.none()
     for node in nodes:
         if node.node_auth_username != "":
             # We have a set username and password to authenticate with this node.
             # We send a GET request to their endpoint.
             url = node.api_url + 'posts'
-            response = requests.get('https://cmput404w20t06.herokuapp.com/api/posts', auth=(
-                node.node_auth_username, node.node_auth_password), headers={'content-type': 'application/json', 'Accept': 'application/json'})
+            # response = requests.get(
+            #     url, auth=(
+            #         node.node_auth_username, node.node_auth_password), headers={'content-type': 'application/json', 'Accept': 'application/json'})
+            response = requests.get(
+                url, headers={'content-type': 'application/json', 'Accept': 'application/json'})
             if response.status_code == 200:
                 # We have the geen light to continue. Otherwise, we just use what we have cached.
                 posts_json = response.json()
@@ -57,14 +55,25 @@ def get_public_posts():
                     author_serializer = AuthorSerializer(data=author)
 
                     if author_serializer.is_valid():
-                        author_serializer.save()
-                    # We now have the author saved, so we can move on to the posts
-                    post = sanitize_post(post)
-                    post_serializer = PostSerializer(data=post)
-                    if post_serializer.is_valid():
-                        post_serializer.save()
-                    else:
-                        print(post_serializer.errors)
-                        print("*****")
-                        print(post)
-                        print("=======")
+                        try:
+                            author_serializer.save()
+                            # We now have the author saved, so we can move on to the posts
+                            post = sanitize_post(post)
+                            post_serializer = PostSerializer(data=post)
+                            if post_serializer.is_valid():
+                                try:
+                                    post_serializer.save()
+                                    print("Loaded post",
+                                          post_serializer.validated_data["title"])
+                                    public_posts = public_posts | Post.objects.filter(
+                                        id=post_serializer.validated_data["id"])
+
+                                except Exception as e:
+                                    print("Error saving post",
+                                          post_serializer.validated_data["title"], str(e))
+                            else:
+                                print("Error encountered:",
+                                      post_serializer.errors)
+                        except Exception as e:
+                            print(e)
+    return public_posts
