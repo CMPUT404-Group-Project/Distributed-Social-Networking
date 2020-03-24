e989d1c8fabcdd2b557145e39d4f31a278e9e35c
Joe Potentier <joseph.potentier@gmail.com>
2020-03-23 20:10:20 -0600

    Fully working version which updates posts when clicked

--- distributedsocialnetwork/post/retrieval.py
+++ distributedsocialnetwork/post/retrieval.py
@@ -5,6 +5,8 @@ from author.serializers import AuthorSerializer
 import requests
 import datetime
 from django.conf import settings
+from .models import Post
+from django.shortcuts import get_object_or_404
 
 
 # The purpose of this is to provide functions that can import Posts from the other Nodes.
@@ -51,9 +53,7 @@ def get_public_posts():
                 posts_json = response.json()
                 for post in posts_json["posts"]:
                     # We first have to ensure the author of each post is in our database.
-                    del post["source"]
-                    post["source"] = settings.FORMATTED_HOST_NAME + \
-                        'posts/' + post['id']
+                    post = transformSource(post)
                     author = sanitize_author(post["author"])
                     author['displayName'] = author['displayName'] + \
                         " (" + node.server_username + ")"
@@ -90,17 +90,51 @@ def get_public_posts():
 
 
 def get_detailed_post(post_id):
-    # get
-    # post_origin = post_origin + '/'
-    # url_parts = post_origin.split('/')
-    # print(url_parts)
-    # node_hostname = 'http://' + url_parts[2] + '/'
-    # post_id
-    # while
+    # Assumes that the post is already in our DB due to the fact that you are passing an ID.
+    local_copy = get_object_or_404(Post, id=post_id)
+    if(local_copy.origin != local_copy.source):
+        local_split = local_copy.origin.split('/')
+        node = Node.objects.get(hostname__contains=local_split[2])
+        url = node.api_url + 'posts/' + local_split[-1]
+        response = requests.get(
+            url, auth=(
+                node.node_auth_username, node.node_auth_password), headers={'content-type': 'application/json', 'Accept': 'application/json'})
+        print(response)
+        if response.status_code == 200:
+            post_json = response.json()
+        print(post_json)
+        post_data = post_json['posts'][0]
+        post_data = transformSource(post_data)
+        post = sanitize_post(post_data)
+        post_serializer = PostSerializer(local_copy, data=post)
+        if post_serializer.is_valid():
+            try:
+                post_serializer.save()
+                print("Updated post",
+                      post_serializer.validated_data["title"])
+                new_copy = get_object_or_404(
+                    Post, id=post_serializer.validated_data["id"])
+                # new_copy = Post.objects.filter(
+                #     id=)
 
-    # post = Post.objects.get(id=post_id)
-    # print(post)
-    # url_to_query = foreign_node + 'posts/' + url_parts[-1]
+            except Exception as e:
+                print("Error saving post",
+                      post_serializer.validated_data["title"], str(e))
+        else:
+            print("Error encountered:",
+                  post_serializer.errors)
+
+        # new_copy = local_copy  # FOR NOW
+        return new_copy
+    else:
+        return local_copy
+    # url =
     # response = requests.get(
-    #     url_to_query, headers={'content-type': 'application/json', 'Accept': 'application/json'})
-    pass
+    #             url, headers={'content-type': 'application/json', 'Accept': 'application/json'})
+
+
+def transformSource(post_obj):
+    del post_obj["source"]
+    post_obj["source"] = settings.FORMATTED_HOST_NAME + \
+        'posts/' + post_obj['id']
+    return post_obj
