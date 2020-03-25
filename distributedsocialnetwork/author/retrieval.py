6f32869987606a011c9922b2185e9d6d3baa25d8
Student <daniel.dick@shaw.ca>
2020-03-25 16:37:32 -0400

    fixes to author retrieval

--- distributedsocialnetwork/author/retrieval.py
+++ distributedsocialnetwork/author/retrieval.py
@@ -1,25 +1,27 @@
 # Serializer for Author
 import requests
-from node.models import Author
+from node.models import Node
 from author.serializers import AuthorSerializer
 from post.retrieval import sanitize_author
 import datetime
+import uuid
 from django.conf import settings
 from .models import Author
 from django.shortcuts import get_object_or_404
+from author.models import Author
 
 def get_detailed_author(author_id):
     local_copy = get_object_or_404(Author, id__icontains=author_id)
     if (local_copy.origin != local_copy.source):
         local_split = local_copy.origin.split('/')
-        node = Node.object.get(hostname__contains=local_split[2])
+        node = Node.objects.get(hostname__contains=local_split[2])
         url = node.api_url + 'author/' + local_split[-1]
         response = requests.get(url, auth=(
             node.node_auth_username, node.node_auth_password), headers={
             'content-type': 'appliation/json', 'Accept': 'application/json'})
         if reponse.status_code == 200:
             author_json = response.json()
-        author_data = author_json['author'][0]
+        author_data = author_json['author']
         author_data = transformSource(author_data)
         author = sanitize_author(author_data)
         author_serializer = AuthorSerializer(local_copy,data=author)
